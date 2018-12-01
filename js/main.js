22eac8dc11c813d6f8fb2aa119f9c6535963868b
Omari Halford <OKHalford@fullsail.edu>
2018-12-01 00:38:16 -0500

    Changed red bar to move left on space key

--- js/main.js
+++ js/main.js
@@ -1,13 +1,18 @@
 let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext("2d");
+let x = 0;
 
 let lastTime = 0
 function render(timeStamp) {
+  ctx.clearRect(0, 0, canvas.clientWidth, canvas.clientHeight);
+  if (spaceKey.isDown) {
+    x++;
+  }
   let dt = timeStamp - lastTime;
   lastTime = timeStamp
   console.log(dt);
   ctx.fillStyle = "#ff0000";
-  ctx.fillRect(20, 20, 100, 50);
+  ctx.fillRect(20 + x, 20, 100, 50);
 
   testFunction();
   window.requestAnimationFrame(render);
