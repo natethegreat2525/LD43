d9ca7b7cc47bbddb63205f71c81f0d3e673cb4e8
Nathan Shirley <natethegreat2525@gmail.com>
2018-12-01 01:39:58 -0500

    Add snow

--- js/main.js
+++ js/main.js
@@ -5,14 +5,19 @@ let x = 0;
 let lastTime = 0
 
 let world = new World(lvl1);
+let snow = new AllSnow(1000, canvas.width, canvas.height);
 
 function render(timeStamp) {
   ctx.clearRect(0, 0, canvas.clientWidth, canvas.clientHeight);
   let dt = timeStamp - lastTime;
   lastTime = timeStamp
 
+  snow.update(dt);
+
   ctx.fillStyle = "rgb(0, 0, 0)";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
+  snow.render();
+
   world.render();
 
   window.requestAnimationFrame(render);
