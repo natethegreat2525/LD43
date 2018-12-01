e3c9713408b5c2bdc796b05d8174409e870bed40
Danny Corliss <dgcorlis@ncsu.edu>
2018-12-01 18:29:59 -0500

    Replace player with santa, add input for down key

--- js/player.js
+++ js/player.js
@@ -1,5 +1,8 @@
+const LEFT = 0;
+const RIGHT = 2;
+const FRONT = 1;
 const PLAYER_WIDTH = 20;
-const PLAYER_HEIGHT = 20;
+const PLAYER_HEIGHT = 47;
 
 class Player {
     constructor(x, y) {
@@ -9,25 +12,154 @@ class Player {
         this.vy = 0;
         this.w = PLAYER_WIDTH;
         this.h = PLAYER_HEIGHT;
+        this.fr = 0;
+        this.dir = FRONT;
     }
 
     render() {
-        ctx.fillStyle = '#ff0000';
-        ctx.fillRect(this.x, this.y, PLAYER_WIDTH, PLAYER_HEIGHT);
+        //Feet
+        let lFoot = null;
+        let rFoot = null;
+	
+        //Left or Right
+        if (this.dir == LEFT || this.dir == RIGHT) {
+            let fcx = this.x+8;
+            let fcy = this.y+44;
+            let ox = Math.sin((this.fr + 4) / 5) * 6;
+            ctx.fillStyle = "#000000";
+            ctx.fillRect(fcx + ox, fcy, 3, 3);
+            ctx.fillRect(fcx - ox, fcy, 3, 3);
+        } else { //Forward
+            let fcxl = this.x+3;
+            let fcxr = this.x+13;
+            let fcy = this.y+43;
+            ctx.fillStyle = "#000000";
+            ctx.fillRect(fcxl, fcy, 4, 4);
+            ctx.fillRect(fcxr, fcy, 4, 4);
+        }
+    
+        //Body
+        ctx.fillStyle = "#000000";
+        ctx.fillRect(this.x, this.y+21, 20, 22); //outline
+        ctx.fillStyle = "#ff0000";
+        ctx.fillRect(this.x+1, this.y+22, 18, 20); //body
+    
+        ctx.fillStyle = "#ffffff";
+        if (this.dir == LEFT) {
+            ctx.fillRect(this.x+4, this.y+21, 4, 20);
+        }
+    
+        if (this.dir == RIGHT) {
+            ctx.fillRect(this.x+12, this.y+21, 4, 20);
+        }
+    
+        if (this.dir == FRONT) {
+            ctx.fillRect(this.x+8, this.y+21, 4, 20);
+        }
+    
+        ctx.fillStyle = "#000000";
+        ctx.fillRect(this.x, this.y+33, 20, 2);  //belt
+        ctx.fillStyle = "#ffffff";
+        ctx.fillRect(this.x, this.y+39, 20, 3);  //horizontal sash
+    
+        //Face
+        ctx.fillStyle = "#FEE3B7";
+        ctx.fillRect(this.x+4, this.y+8, 13, 13); //face
+    
+        //Hat 
+        ctx.beginPath();
+        ctx.moveTo(this.x+12, this.y+1);
+        ctx.lineTo(this.x+3, this.y+7);
+        ctx.lineTo(this.x+17, this.y+7);
+        ctx.closePath(); //hat shape
+        
+        ctx.lineWidth = 1;
+        ctx.strokeStyle = '#000000';
+        ctx.stroke(); //hat outline
+        
+        ctx.fillStyle = "#ff0000";
+        ctx.fill(); //hat triangle
+    
+        ctx.fillStyle = "#ffffff";
+        ctx.fillRect(this.x+11, this.y+0, 3, 3); //hat pompom
+    
+        ctx.fillStyle = "#000000";
+        ctx.fillRect(this.x+2, this.y+7, 16, 2);  //hat stripe outline
+        
+        ctx.fillStyle = "#ffffff";
+        ctx.fillRect(this.x+3, this.y+7, 14, 2);  //hat stripe
+    
+        if (this.dir == LEFT) {
+            //Eyes
+            ctx.fillStyle = "#000000";
+            ctx.fillRect(this.x+5, this.y+11, 3, 3); //eye
+        
+            //Beard Position
+            ctx.beginPath();
+            ctx.moveTo(this.x+7, this.y+30);
+            ctx.lineTo(this.x+4, this.y+18);
+            ctx.lineTo(this.x+10, this.y+18);
+            ctx.closePath(); //beard shape
+        }
+    
+        if (this.dir == RIGHT) {
+                //Eyes
+
+            ctx.fillStyle = "#000000";
+            ctx.fillRect(this.x+13, this.y+11, 3, 3); //eye
+        
+            //Beard Position
+            ctx.beginPath();
+            ctx.moveTo(this.x+15, this.y+30);
+            ctx.lineTo(this.x+11, this.y+18);
+            ctx.lineTo(this.x+17, this.y+18);
+            ctx.closePath(); //beard shape
+        }
+    
+        if (this.dir == FRONT) {
+                //Eyes
+
+            ctx.fillStyle = "#000000";
+            ctx.fillRect(this.x+5, this.y+12, 3, 3); //left eye
+            ctx.fillRect(this.x+13, this.y+12, 3, 3); //right eye
+        
+            //Beard Position
+            ctx.beginPath();
+            ctx.moveTo(this.x+11, this.y+30);
+            ctx.lineTo(this.x+5, this.y+18);
+            ctx.lineTo(this.x+16, this.y+18);
+            ctx.closePath(); //beard shape
+        }
+    
+        //Beard 
+        ctx.lineWidth = 1;
+        ctx.strokeStyle = '#B7B7B7';
+        ctx.stroke(); //beard outline
+        
+        ctx.fillStyle = "#E2E2E2";
+        ctx.fill(); //beard triangle
+        return;
     }
 
-    update() {
+    update(dt) {
         if (leftKey.isDown || aKey.isDown) { 
             this.vx = -200;
-        }
-        else if (rightKey.isDown || dKey.isDown) {
+            this.dir = LEFT;
+        } else if (rightKey.isDown || dKey.isDown) {
             this.vx = 200;
+            this.dir = RIGHT;
         } else {
             this.vx *= .7;
+            if ((downKey.isDown || sKey.isDown) && this.grounded) {
+                this.dir = FRONT;
+            }
         }
 
         if ((upKey.isDown || wKey.isDown) && this.grounded) {
             this.vy = -300;
+            this.dir = FRONT;
         }
+        
+        this.fr = dt;
     }
 }
--- js/santa.js
+++ js/santa.js
@@ -1,127 +1,157 @@
-function makeSanta(x, y, dir, fr) {
 
-    ctx.clearRect(0,0,50,50);
-	
-	//Feet
-	let lFoot = null;
-	let rFoot = null;
-	
-	//Left or Right
-	if (dir == LEFT || dir == RIGHT) {
-		let fcx = 8;
-		let fcy = 44;
-		let ox = Math.sin((fr + 4) / 5) * 6;
-		ctx.fillStyle = "#000000";
-		ctx.fillRect(fcx + ox, fcy, 3, 3);
-		ctx.fillRect(fcx - ox, fcy, 3, 3);
-	} else { //Forward
-		let fcxl = 3;
-		let fcxr = 13;
-		let fcy = 43;
-		ctx.fillStyle = "#000000";
-		ctx.fillRect(fcxl, fcy, 4, 4);
-		ctx.fillRect(fcxr, fcy, 4, 4);
-	}
-	
-	//Body
-	ctx.fillStyle = "#000000";
-	ctx.fillRect(0, 21, 20, 22); //outline
-    ctx.fillStyle = "#ff0000";
-	ctx.fillRect(1, 22, 18, 20); //body
-	
-	ctx.fillStyle = "#ffffff";
-	if (dir == LEFT) {
-	    ctx.fillRect(4, 21, 4, 20);
-	}
-	
-	if (dir == RIGHT) {
-	    ctx.fillRect(12, 21, 4, 20);
-	}
-	
-	if (dir == FRONT) {
-		ctx.fillRect(8, 21, 4, 20);
-	}
-	
-	ctx.fillStyle = "#000000";
-	ctx.fillRect(0, 33, 20, 2);  //belt
-	ctx.fillStyle = "#ffffff";
-	ctx.fillRect(0, 39, 20, 3);  //horizontal sash
-	
-	//Face
-	ctx.fillStyle = "#FEE3B7";
-	ctx.fillRect(4, 8, 13, 13); //face
-	
-	//Hat 
-	ctx.beginPath();
-    ctx.moveTo(12, 1);
-    ctx.lineTo(3, 7);
-    ctx.lineTo(17, 7);
-    ctx.closePath(); //hat shape
-        
-    ctx.lineWidth = 1;
-    ctx.strokeStyle = '#000000';
-    ctx.stroke(); //hat outline
-        
-    ctx.fillStyle = "#ff0000";
-    ctx.fill(); //hat triangle
+
+class Santa {
+    constructor(x, y, dir) {
+        this.x = x;
+        this.y = y;
+        this.vx = 0;
+        this.vy = 0;
+        this.fr = 0;
+        this.dir = FRONT;
+    }
+
+    render() {
     
-    ctx.fillStyle = "#ffffff";
-    ctx.fillRect(11, 0, 3, 3); //hat pompom
+        //Feet
+        let lFoot = null;
+        let rFoot = null;
+	
+        //Left or Right
+        if (this.dir == LEFT || this.dir == RIGHT) {
+            let fcx = this.x+8;
+            let fcy = this.y+44;
+            let ox = Math.sin((this.fr + 4) / 5) * 6;
+            ctx.fillStyle = "#000000";
+            ctx.fillRect(fcx + ox, fcy, 3, 3);
+            ctx.fillRect(fcx - ox, fcy, 3, 3);
+        } else { //Forward
+            let fcxl = this.x+3;
+            let fcxr = this.x+13;
+            let fcy = this.y+43;
+            ctx.fillStyle = "#000000";
+            ctx.fillRect(fcxl, fcy, 4, 4);
+            ctx.fillRect(fcxr, fcy, 4, 4);
+        }
     
-    ctx.fillStyle = "#000000";
-	ctx.fillRect(2, 7, 16, 2);  //hat stripe outline
+        //Body
+        ctx.fillStyle = "#000000";
+        ctx.fillRect(this.x, this.y+21, 20, 22); //outline
+        ctx.fillStyle = "#ff0000";
+        ctx.fillRect(this.x+1, this.y+22, 18, 20); //body
+    
+        ctx.fillStyle = "#ffffff";
+        if (this.dir == LEFT) {
+            ctx.fillRect(this.x+4, this.y+21, 4, 20);
+        }
+    
+        if (this.dir == RIGHT) {
+            ctx.fillRect(this.x+12, this.y+21, 4, 20);
+        }
+    
+        if (this.dir == FRONT) {
+            ctx.fillRect(this.x+8, this.y+21, 4, 20);
+        }
+    
+        ctx.fillStyle = "#000000";
+        ctx.fillRect(this.x, this.y+33, 20, 2);  //belt
+        ctx.fillStyle = "#ffffff";
+        ctx.fillRect(this.x, this.y+39, 20, 3);  //horizontal sash
+    
+        //Face
+        ctx.fillStyle = "#FEE3B7";
+        ctx.fillRect(this.x+4, this.y+8, 13, 13); //face
+    
+        //Hat 
+        ctx.beginPath();
+        ctx.moveTo(this.x+12, this.y+1);
+        ctx.lineTo(this.x+3, this.y+7);
+        ctx.lineTo(this.x+17, this.y+7);
+        ctx.closePath(); //hat shape
         
-    ctx.fillStyle = "#ffffff";
-	ctx.fillRect(3, 7, 14, 2);  //hat stripe
-	
-	if (dir == LEFT) {
-        //Eyes
-	    ctx.fillStyle = "#000000";
-	    ctx.fillRect(5, 11, 3, 3); //eye
-	    
-		//Beard Position
-        ctx.beginPath();
-        ctx.moveTo(7, 30);
-        ctx.lineTo(4, 18);
-        ctx.lineTo(10, 18);
-        ctx.closePath(); //beard shape
-	}
-	
-	if (dir == RIGHT) {
-	        //Eyes
-
-		ctx.fillStyle = "#000000";
-		ctx.fillRect(13, 11, 3, 3); //eye
-		
-		//Beard Position
-        ctx.beginPath();
-        ctx.moveTo(15, 30);
-        ctx.lineTo(11, 18);
-        ctx.lineTo(17, 18);
-        ctx.closePath(); //beard shape
-	}
-	
-	if (dir == FRONT) {
-	        //Eyes
-
-	    ctx.fillStyle = "#000000";
-		ctx.fillRect(5, 12, 3, 3); //left eye
-		ctx.fillRect(13, 12, 3, 3); //right eye
-		
-		//Beard Position
-        ctx.beginPath();
-        ctx.moveTo(11, 30);
-        ctx.lineTo(5, 18);
-        ctx.lineTo(16, 18);
-        ctx.closePath(); //beard shape
-	}
-	
-	//Beard 
-	ctx.lineWidth = 1;
-    ctx.strokeStyle = '#B7B7B7';
-    ctx.stroke(); //beard outline
+        ctx.lineWidth = 1;
+        ctx.strokeStyle = '#000000';
+        ctx.stroke(); //hat outline
         
-    ctx.fillStyle = "#E2E2E2";
-    ctx.fill(); //beard triangle
-	return;
+        ctx.fillStyle = "#ff0000";
+        ctx.fill(); //hat triangle
+    
+        ctx.fillStyle = "#ffffff";
+        ctx.fillRect(this.x+11, this.y+0, 3, 3); //hat pompom
+    
+        ctx.fillStyle = "#000000";
+        ctx.fillRect(this.x+2, this.y+7, 16, 2);  //hat stripe outline
+        
+        ctx.fillStyle = "#ffffff";
+        ctx.fillRect(this.x+3, this.y+7, 14, 2);  //hat stripe
+    
+        if (this.dir == LEFT) {
+            //Eyes
+            ctx.fillStyle = "#000000";
+            ctx.fillRect(this.x+5, this.y+11, 3, 3); //eye
+        
+            //Beard Position
+            ctx.beginPath();
+            ctx.moveTo(this.x+7, this.y+30);
+            ctx.lineTo(this.x+4, this.y+18);
+            ctx.lineTo(this.x+10, this.y+18);
+            ctx.closePath(); //beard shape
+        }
+    
+        if (this.dir == RIGHT) {
+                //Eyes
+
+            ctx.fillStyle = "#000000";
+            ctx.fillRect(this.x+13, this.y+11, 3, 3); //eye
+        
+            //Beard Position
+            ctx.beginPath();
+            ctx.moveTo(this.x+15, this.y+30);
+            ctx.lineTo(this.x+11, this.y+18);
+            ctx.lineTo(this.x+17, this.y+18);
+            ctx.closePath(); //beard shape
+        }
+    
+        if (this.dir == FRONT) {
+                //Eyes
+
+            ctx.fillStyle = "#000000";
+            ctx.fillRect(this.x+5, this.y+12, 3, 3); //left eye
+            ctx.fillRect(this.x+13, this.y+12, 3, 3); //right eye
+        
+            //Beard Position
+            ctx.beginPath();
+            ctx.moveTo(this.x+11, this.y+30);
+            ctx.lineTo(this.x+5, this.y+18);
+            ctx.lineTo(this.x+16, this.y+18);
+            ctx.closePath(); //beard shape
+        }
+    
+        //Beard 
+        ctx.lineWidth = 1;
+        ctx.strokeStyle = '#B7B7B7';
+        ctx.stroke(); //beard outline
+        
+        ctx.fillStyle = "#E2E2E2";
+        ctx.fill(); //beard triangle
+        return;
+    }
+    
+    update(dt) {
+        if (leftKey.isDown || aKey.isDown) { 
+            this.vx = -200;
+            this.dir = LEFT;
+        }
+        else if (rightKey.isDown || dKey.isDown) {
+            this.vx = 200;
+            this.dir = RIGHT;
+        } else {
+            this.vx *= .7;
+        }
+
+        if ((upKey.isDown || wKey.isDown) && this.grounded) {
+            this.vy = -300;
+            this.dir = FRONT;
+        }
+        this.fr = dt;
+    }
 }
