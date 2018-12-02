d9ee29893f29efc99bdb5f27d2566c4e76695455
Nathan Shirley <natethegreat2525@gmail.com>
2018-12-02 02:35:33 -0500

    Entity collisions

--- js/physics.js
+++ js/physics.js
@@ -21,6 +21,63 @@ class Physics {
                 ent.vy += GRAVITY * dt;
             }
         }
+
+        for (let i = 0; i < this.entities.length; i++) {
+            for (let j = i+1; j < this.entities.length; j++) {
+                let e1 = this.entities[i];
+                let e2 = this.entities[j];
+                let overlap = getRectOverlap(e1.x, e1.y, e1.w, e1.h, e2.x, e2.y, e2.w, e2.h);
+                if (overlap === null) {
+                    continue;
+                }
+                let hlfx = overlap[0]/2;
+                let hlfy = overlap[1]/2;
+
+                let dontHandle = false;
+                if (e1.handleEntityCollision) {
+                    if (e1.handleEntityCollision(e2) === false) {
+                        dontHandle = true;
+                    }
+                }
+                if (e2.handleEntityCollision) {
+                    if (e2.handleEntityCollision(e1) === false) {
+                        dontHandle = true;
+                    }
+                }
+
+                if (dontHandle) {
+                    continue;
+                }
+
+                let midvx = (e1.vx + e2.vx) / 2;
+                let midvy = (e1.vy + e2.vy) / 2;
+
+                if (hlfy !== 0) {
+                    // vertical collision
+                    if (hlfy > 0) {
+                        e1.vy = Math.max(midvy, e1.vy);
+                        e2.vy = Math.min(midvy, e2.vy);
+                    } else {
+                        e1.vy = Math.min(midvy, e1.vy);
+                        e2.vy = Math.max(midvy, e2.vy);
+                    }
+                } else {
+                    // horizontal collision
+                    if (hlfx > 0) {
+                        e1.vx = Math.max(midvx, e1.vx);
+                        e2.vx = Math.min(midvx, e2.vx);
+                    } else {
+                        e1.vx = Math.min(midvx, e1.vx);
+                        e2.vx = Math.max(midvx, e2.vx);
+                    }
+                }
+                e1.x += hlfx;
+                e1.y += hlfy;
+
+                e2.x -= hlfx;
+                e2.y -= hlfy;
+            }
+        }
         for (let idx in this.entities) {
             let ent = this.entities[idx];
 
@@ -110,6 +167,9 @@ function getRectOverlap(x1, y1, w1, h1, x2, y2, w2, h2) {
 
     let dx = mx1 - mx2;
     let dy = my1 - my2;
+    if (Math.abs(dx) > (w1 + w2) / 2 || Math.abs(dy) > (h1 + h2) / 2) {
+        return null;
+    }
     if (Math.abs(dx) - (w1 + w2) / 2 > Math.abs(dy) - (h1 + h2) / 2) {
         if (dx > 0) {
             dx -= (w1 + w2) / 2;
