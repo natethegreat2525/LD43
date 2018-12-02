a4c755f03b4335ec72926ba3501f790c7100ac3d
Danny Corliss <dgcorlis@ncsu.edu>
2018-12-01 23:38:11 -0500

    background color, wide elves

--- js/elf.js
+++ js/elf.js
@@ -7,8 +7,20 @@ class Elf {
         this.y = y;
         this.vx = 0;
         this.vy = 0;
-        this.w = ELF_WIDTH;
-        this.h = ELF_HEIGHT;
+        if (!h) {
+            this.h = ELF_HEIGHT;
+        } else {
+            if (h < 8) {
+                this.h = 8;
+            } else {
+                this.h = h;
+            }
+        }
+        if (!w) {
+            this.w = ELF_WIDTH;
+        } else {
+            this.w = w;
+        }
         this.fr = 0;
         this.dir = FRONT;
     }
@@ -21,15 +33,15 @@ class Elf {
     
         //Left or Right
         if (this.dir == LEFT || this.dir == RIGHT) {
-            let fcx = this.x+6;
+            let fcx = this.x+(this.w/2);
             let fcy = this.y+38;
             let ox = Math.sin((this.fr + 4) / 5) * 6;
             ctx.fillStyle = "#000000";
             ctx.fillRect(fcx + ox, fcy, 3, 3);
             ctx.fillRect(fcx - ox, fcy, 3, 3);
         } else { //Forward
-            let fcxl = this.x+1;
-            let fcxr = this.x+11;
+            let fcxl = this.x+(this.w/2 - this.w/6)-2;
+            let fcxr = this.x+(this.w/2 + this.w/6)-2;
             let fcy = this.y+37;
             ctx.fillStyle = "#000000";
             ctx.fillRect(fcxl, fcy, 4, 4);
@@ -38,38 +50,39 @@ class Elf {
     
         //Body
         ctx.fillStyle = "#137703";
-        ctx.fillRect(this.x, this.y+19, 16, 16); //outline
+        ctx.fillRect(this.x, this.y+19, this.w, 16); //outline
         ctx.fillStyle = "#02A42C";
-        ctx.fillRect(this.x+1, this.y+20, 14, 14); //body
+        ctx.fillRect(this.x+1, this.y+20, this.w-2, 14); //body
     
+        //Vertical sash
         ctx.fillStyle = "#ff0000";
         if (this.dir == LEFT) {
-            ctx.fillRect(this.x+2, this.y+17, 4, 18);
+            ctx.fillRect(this.x+((this.w-2)/4), this.y+19, 4, 16);
         }
     
         if (this.dir == RIGHT) {
-            ctx.fillRect(this.x+10, this.y+17, 4, 18);
+            ctx.fillRect(this.x+((this.w-2)*3/4), this.y+19, 4, 16);
         }
     
         if (this.dir == FRONT) {
-            ctx.fillRect(this.x+6, this.y+17, 4, 18);
+            ctx.fillRect(this.x+((this.w-2)/2), this.y+19, 4, 16);
         }
     
         ctx.fillStyle = "#ff0000";
-        ctx.fillRect(this.x+0, this.y+33, 16, 3);  //horizontal sash
+        ctx.fillRect(this.x+0, this.y+33, this.w, 3);  //horizontal sash
     
         //Face
         ctx.fillStyle = "#FEE3B7";
-        ctx.fillRect(this.x+2, this.y+8, 11, 11); //face
+        ctx.fillRect(this.x+(this.w/2 - 11/2), this.y+8, 11, 11); //face
     
         //Ears
         if (this.dir == FRONT) {
     
             //Left Ear
             ctx.beginPath();
-            ctx.moveTo(this.x+2, this.y+12);
-            ctx.lineTo(this.x+2, this.y+14);
-            ctx.lineTo(this.x, this.y+10);
+            ctx.moveTo(this.x+(this.w/2 - 11/2)-1, this.y+12);
+            ctx.lineTo(this.x+(this.w/2 - 11/2)-1, this.y+14);
+            ctx.lineTo(this.x+(this.w/2 - 11/2)-3, this.y+10);
             ctx.closePath();
         
             ctx.fillStyle = "#FCD179";
@@ -77,9 +90,9 @@ class Elf {
         
             //Right Ear
             ctx.beginPath();
-            ctx.moveTo(this.x+13, this.y+12);
-            ctx.lineTo(this.x+13, this.y+14);
-            ctx.lineTo(this.x+15, this.y+10);
+            ctx.moveTo(this.x+(this.w/2 - 11/2)+12, this.y+12);
+            ctx.lineTo(this.x+(this.w/2 - 11/2)+12, this.y+14);
+            ctx.lineTo(this.x+(this.w/2 - 11/2)+14, this.y+10);
             ctx.closePath();
         
             ctx.fillStyle = "#FCD179";
@@ -88,9 +101,9 @@ class Elf {
 
         //Hat 
         ctx.beginPath();
-        ctx.moveTo(this.x+9, this.y+1);
-        ctx.lineTo(this.x+2, this.y+7);
-        ctx.lineTo(this.x+13, this.y+7);
+        ctx.moveTo(this.x+(this.w/2 - 11/2)+7, this.y+1);
+        ctx.lineTo(this.x+(this.w/2 - 11/2), this.y+7);
+        ctx.lineTo(this.x+(this.w/2 - 11/2)+11, this.y+7);
         ctx.closePath(); //hat shape
         
         ctx.lineWidth = 1;
@@ -101,40 +114,40 @@ class Elf {
         ctx.fill(); //hat triangle
     
         ctx.fillStyle = "#ff0000";
-        ctx.fillRect(this.x+9, this.y+0, 3, 3); //hat pompom
+        ctx.fillRect(this.x+(this.w/2 - 3/2)+2, this.y+0, 3, 3); //hat pompom
     
         ctx.fillStyle = "#000000";
-        ctx.fillRect(this.x+1, this.y+7, 13, 2);  //hat stripe outline
+        ctx.fillRect(this.x+(this.w/2 - 13/2), this.y+7, 13, 2);  //hat stripe outline
         
         ctx.fillStyle = "#ff0000";
-        ctx.fillRect(this.x+1, this.y+7, 13, 2);  //hat stripe
+        ctx.fillRect(this.x+(this.w/2 - 13/2), this.y+7, 13, 2);  //hat stripe
     
         if (this.dir == LEFT) {
             //Eyes
             ctx.fillStyle = "#000000";
-            ctx.fillRect(this.x+3, this.y+12, 2, 2); //eye    
+            ctx.fillRect(this.x+(this.w/2 - 4), this.y+12, 2, 2); //eye    
         }
     
         if (this.dir == RIGHT) {
                 //Eyes
 
             ctx.fillStyle = "#000000";
-            ctx.fillRect(this.x+11, this.y+12, 2, 2); //eye
+            ctx.fillRect(this.x+(this.w/2 + 2), this.y+12, 2, 2); //eye
         }
     
         if (this.dir == FRONT) {
                 //Eyes
 
             ctx.fillStyle = "#000000";
-            ctx.fillRect(this.x+4, this.y+12, 2, 2); //left eye
-            ctx.fillRect(this.x+9, this.y+12, 2, 2); //right eye
+            ctx.fillRect(this.x+(this.w/2 - 4), this.y+12, 2, 2); //left eye
+            ctx.fillRect(this.x+(this.w/2 + 2), this.y+12, 2, 2); //right eye
         }
 	
 	    return;
 	}
 	
 	update(dt) {
-        this.dir = FRONT;
+        this.dir = RIGHT;
         this.fr++;
     }
 }
